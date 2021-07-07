9b130f4f8e6672b26dcf4bc0a905a31d23d008fb
Akash Dutta <akashduttaphp.ad@gmail.com>
2021-07-08 01:57:18 +0530

    update 08July

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component,HostListener } from '@angular/core';
 import { ApiService } from './services/api.service';
+import { Router } from '@angular/router';
 import { UtilService } from './services/util.service';
 @Component({
   selector: 'app-root',
@@ -11,7 +12,31 @@ export class AppComponent {
   deviceType = 'desktop';
   innerHeight: string;
 
-  constructor(public api: ApiService, public util: UtilService) {
+  constructor(public api: ApiService, public util: UtilService,private router: Router) {
     console.log(this.util.deviceType);
+    
   }
+  
+  // Keep me Signed in
+  // public doUnload(): void {
+  //   this.doBeforeUnload();
+  // }
+
+  // // Keep me Signed in
+  // public doBeforeUnload(): void {
+  //   localStorage.clear();
+  //   this.util.userInfo = null;
+  //   this.router.navigate(['/']).then(() => {
+  //     window.location.reload();
+  //   });
+  // }
+
+  // @HostListener('window:beforeunload', ['$event'])
+  //  onWindowClose(event: any): void {
+  //   // Do something
+  //   alert();
+  //    event.preventDefault();
+  //    event.returnValue = false;
+
+  // }
 }
